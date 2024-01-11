7dd72a135123e244e348f31331f55e1c61ded26c
Denis Vermylen <dve@odoo.com>
2024-01-11 11:43:05 +0100

    Update __manifest__.py

--- birthdays/__manifest__.py
+++ birthdays/__manifest__.py
@@ -13,7 +13,7 @@
     'website': "www.odoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check  https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Administration',
     'version': '0.1',
